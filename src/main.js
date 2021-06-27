004cabb615ac9635f1c529fd1b4c66903bc1367b
Josephine Dujardin <josephine83230@gmail.com>
2021-06-27 18:58:04 +0200

    challenge S3

--- src/main.js
+++ src/main.js
@@ -10,72 +10,72 @@ import 'element-plus/lib/theme-chalk/index.css'
 const LoginPage = () => import('./pages/Login.vue')
 const NotFoundPage = () => import('./pages/NotFound.vue')
 const SettingsPage = () => import('./pages/Settings.vue')
-const SettingsUser = () => import('./components/SettingsUser.vue')
 const SettingsApp = () => import('./components/SettingsApp.vue')
+const SettingsUser = () => import('./components/SettingsUser.vue')
 
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
-    routes: [
+  history: VueRouter.createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      alias: '/home',
+      name: 'Home',
+      component: HomePage,
+      meta: { needJsonBin: true },
+      children: [
         {
-            path: '/',
-            alias: '/home',
-            name: 'Home',
-            component: HomePage,
-            meta: { needLoggedIn: false },
-            children: [
-                {
-                    path: 'home/:taskID',
-                    component: HomePage
-                },
-            ]
-        },      
-        {
-            path: '/settings',
-            name: 'Settings',
-            component: SettingsPage,
-            meta: { needLoggedIn: false },
-            children: [
-                {
-                    path: 'app',
-                    component: SettingsApp,
-                    meta: { needLoggedIn: false }
-                },
-                {
-                    path: 'user',
-                    component: SettingsUser,
-                    meta: { needLoggedIn: false }
-                }
-            ]
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: LoginPage,
-            beforeEnter: (to , from) => {
-                if (localStorage.getItem('isLoggedIn')) {
-                    return '/'
-                }
-            }
-        },
-        {
-            path: '/notfound',
-            name: 'NotFound',
-            component: NotFoundPage
-        },
-        {
-            path: '/:wrongPath(.*)',
-            redirect: (to) => {
-                return { name: "NotFound", params: { wrongPath: to.params.wrongPath } }
-            }
+          path: 'home/:taskID',
+          component: HomePage
         }
-    ]
+      ]
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: SettingsPage,
+      meta: { needJsonBin: true },
+      children: [
+        {
+          path: 'app',
+          component: SettingsApp,
+          meta: { needJsonBin: false }
+        },
+        {
+          path: 'user',
+          component: SettingsUser,
+          meta: { needJsonBin: true }
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage,
+      beforeEnter: (to, from) => {
+        if (localStorage.getItem('jsonBinAccess')) {
+          return '/'
+        }
+      }
+    },
+    {
+      path: '/notfound',
+      name: 'NotFound',
+      component: NotFoundPage
+    },
+    {
+      path: '/:wrongPath(.*)',
+      redirect: (to) => {
+        return { name: 'NotFound', params: { wrongPath: to.params.wrongPath } }
+      }
+    }
+  ]
 })
 
 router.beforeEach((to, from) => {
-    /* global localStorage */
-    if (to.meta.needLoggedIn && !localStorage.getItem('isLoggedIn')) {
-        return '/login'        
-    }
+  /* global localStorage */
+  if (to.meta.needJsonBin && !localStorage.getItem('jsonBinAccess')) {
+    return '/settings/app'
+  }
 })
 
 const app = createApp(App)
